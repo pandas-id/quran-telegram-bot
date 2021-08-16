772ba4bc5016ea2dc4b4848cd2dd94f3b7c89776
Akmal <pandasid05@gmail.com>
2021-08-16 21:03:46 +0800

    change host

--- app.py
+++ app.py
@@ -276,4 +276,4 @@ if __name__ == '__main__':
     except telepot.exception.TooManyRequestsError:
         pass
     # start
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host=HOST, port=int(os.environ.get('PORT', 5000)), debug=True)
