5802812e9b60e6253e17b286790fa4c5829da6f3
Akmal <pandasid05@gmail.com>
2022-09-25 13:44:55 +0800

    debugging code

--- app.py
+++ app.py
@@ -273,7 +273,7 @@ def index():
 if __name__ == '__main__':
     try:
         bot.setWebhook(HOST)
-    except telepot.exception.TooManyRequestsError:
+    except exception.TooManyRequestsError:
         pass
     # start
     web.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
